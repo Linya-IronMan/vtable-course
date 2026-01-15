a7e327cad5c889aea232ddcfde928ff14780c2d3
林崖 <cdthanye32@163.com>
2026-01-15 09:14:09 +0800

    feat(table-editor-base): 优化代码

---
+++ examples/table-editor/constants.ts
@@ -0,0 +1,12 @@
+export enum TableEditorType {
+	NAME_EDITOR = "name-editor",
+	TEXT_EDITOR = "textArea-editor",
+	NUMBER_EDITOR = "number-editor",
+	DATE_EDITOR = "date-editor",
+	LIST_EDITOR = "list-editor",
+	CUSTOM_DATE_EDITOR = "custom-date",
+}
+
+export enum TableChartModule {
+	VCHART = "vchart",
+}
--- examples/table-editor/registry.ts
+++ examples/table-editor/registry.ts
@@ -8,6 +8,7 @@ import {
 import { InputEditor } from "@visactor/vtable-editors";
 
 import { DateEditor } from "./editor/date-editor";
+import { TableChartModule, TableEditorType } from "./constants";
 
 const inputEditor = new InputEditor();
 const textAreaEditor = new TextAreaEditor();
@@ -15,11 +16,11 @@ const dateInputEditor = new DateInputEditor();
 const listEditor = new ListEditor({ values: ["女", "男"] });
 const custom_date_editor = new DateEditor({});
 
-register.editor("name-editor", inputEditor);
-register.editor("textArea-editor", textAreaEditor);
-register.editor("date-editor", dateInputEditor);
-register.editor("list-editor", listEditor);
-register.editor("custom-date", custom_date_editor);
+register.editor(TableEditorType.NAME_EDITOR, inputEditor);
+register.editor(TableEditorType.TEXT_EDITOR, textAreaEditor);
+register.editor(TableEditorType.DATE_EDITOR, dateInputEditor);
+register.editor(TableEditorType.LIST_EDITOR, listEditor);
+register.editor(TableEditorType.CUSTOM_DATE_EDITOR, custom_date_editor);
 
 // 注册 vchart 图表模块
-register.chartModule("vchart", VChart);
+register.chartModule(TableChartModule.VCHART, VChart);
