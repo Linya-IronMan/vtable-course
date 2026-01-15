6159ff450499b75e20b764d6853f1f970d597d17
林崖 <cdthanye32@163.com>
2026-01-15 08:29:18 +0800

    feat(table-editor-base): chart 渲染，添加注释

--- examples/table-editor/data/records.ts
+++ examples/table-editor/data/records.ts
@@ -6,18 +6,16 @@ export const records = [
 		address: "中国 北京",
 		birthDate: "2000-01-01",
 		customDate: "2023-01-01",
-		areaChart: {
-			chartData: [
-				// 图表数据数组
-				{ x: "0", type: "A", y: 100 },
-				{ x: "1", type: "A", y: 130 },
-				{ x: "2", type: "A", y: 130 },
-				{ x: "3", type: "A", y: 130 },
-				{ x: "4", type: "A", y: 130 },
-				{ x: "5", type: "A", y: 130 },
-				{ x: "6", type: "A", y: 130 },
-				// ...更多数据
-			],
-		},
+		areaChart: [
+			// 图表数据数组
+			{ x: "0", type: "A", y: 100 },
+			{ x: "1", type: "A", y: 130 },
+			{ x: "2", type: "A", y: 130 },
+			{ x: "3", type: "A", y: 130 },
+			{ x: "4", type: "A", y: 130 },
+			{ x: "5", type: "A", y: 130 },
+			{ x: "6", type: "A", y: 130 },
+			// ...更多数据
+		],
 	},
 ];
