70ca27bd75c8e626cdb78b67e23f9b3dfeab97f1
林崖 <cdthanye32@163.com>
2026-01-29 17:05:43 +0800

    feat(table-editor-base): icon 注册与使用

--- examples/table-editor/icon/setup.ts
+++ examples/table-editor/icon/setup.ts
@@ -1,3 +1,5 @@
+import { ICON_FONT_FAMILY } from "./constants";
+
 const iconPool = [
 	"url('https://at.alicdn.com/t/c/font_5078475_05rtxdjro3ht.woff2?t=1769122605954') format('woff2')",
 	"url('https://at.alicdn.com/t/c/font_5078475_05rtxdjro3ht.woff?t=1769122605954') format('woff')",
@@ -5,7 +7,7 @@ const iconPool = [
 	"url('https://at.alicdn.com/t/c/font_5078475_05rtxdjro3ht.svg?t=1769122605954#iconfont') format('svg')",
 ];
 export const iconSetup = async () => {
-	const face = new FontFace("base-iconfont", iconPool.join(", "));
+	const face = new FontFace(ICON_FONT_FAMILY, iconPool.join(", "));
 	const loaded = await face.load();
-	(document as any).fonts.add(loaded);
+	document.fonts.add(loaded);
 };
--- examples/table-editor/icon/text-icon.ts
+++ examples/table-editor/icon/text-icon.ts
@@ -1,5 +1,6 @@
 import { TYPES } from "@visactor/vtable";
 import { TextIcon } from "@visactor/vtable/es/ts-types";
+import { ICON_FONT_FAMILY } from "./constants";
 
 const icon = String.fromCodePoint(parseInt("\e6a5", 16));
 export const textIcon: TextIcon = {
@@ -10,7 +11,7 @@ export const textIcon: TextIcon = {
 	marginLeft: 10,
 	style: {
 		cursor: "pointer",
-		fontFamily: "base-iconfont",
+		fontFamily: ICON_FONT_FAMILY,
 		width: 20,
 		height: 20,
 		fontSize: 20,
